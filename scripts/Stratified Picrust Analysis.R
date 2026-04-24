f12f51345cef48c2c7d4408ffd3646fa51f11139
Samantha Donato <159091186+samanthad27@users.noreply.github.com>
2026-04-23 20:03:33 -0700

    Enhance taxonomic contribution analysis in script
    Updated the script to include additional functionality for analyzing taxonomic contributions to pathways, including filtering and aggregating data for different swim performance groups.

--- scripts/Stratified Picrust Analysis.R
+++ scripts/Stratified Picrust Analysis.R
@@ -45,22 +45,23 @@ energy_pathways <- c(
 # * Convert to relative abundance to control for sequencing depth
 
 # Load Fish Metadata 
-metadata <- read_excel("datasets/fish_metadata_2.xlsx")
+metadata <- read_excel("fish_metadata_2.xlsx")
 
 # Load stratified pathway contribution data (MetaCyc used)
-strat_mc_data_1 <- data.table::fread("datasets/path_abun_contrib.tsv")
+strat_mc_data_1 <- data.table::fread("path_abun_contrib.tsv")
 
 # Rename column name from #SampleID to sample 
 metadata <- metadata %>%
   rename(sample = `#SampleID`)
 
 # Left join sample names from metadata to stratified MetaCyc data
-strat_mc_data_1 <- data.table::fread("datasets/path_abun_contrib.tsv") %>%
+strat_mc_data_1 <- data.table::fread("path_abun_contrib.tsv") %>%
   left_join(metadata, by = "sample")
 
 # Filter for hindgut samples
 strat_hindgut <- strat_mc_data_1 %>%
   filter(sample_type == "hindgut")
+
 
 # Filter for fast, moderate, slow groups
 strat_fast <- strat_hindgut %>%
@@ -88,6 +89,11 @@ strat_rel_slow = strat_slow %>%
   mutate(rel_abun = taxon_function_abun/sum(taxon_function_abun)) %>%
   ungroup()
 
+# If we want to analyze everything without subsetting groups
+strat_rel = strat_hindgut %>%
+  group_by(sample())
+  mutate(rel_abun = taxon_function_abun/sum(taxon_function_abun)) %>%
+  ungroup()
 
 # Only keep the pathways of interest
 strat_filt_fast = strat_rel_fast %>% 
@@ -102,10 +108,13 @@ strat_filt_slow = strat_rel_slow %>%
   # Only include pathways of interest
   filter(`function` %in% energy_pathways)
 
+strat_filt = strat_rel %>% 
+  # Only include pathways of interest
+  filter(`function` %in% energy_pathways)
 
 # Aggregate reads to the Family level
 # Load tax data, keep just the Family info
-tax <- read_tsv("datasets/data_taxonomy.tsv") %>%  # Load Taxonomy Data Names
+tax <- read_tsv("data_taxonomy.tsv") %>%  # Load Taxonomy Data Names
   separate(`Taxon`, into = c("Kingdom", "Phylum", "Class", "Order", "Family", "Genus"), sep = ";") %>%
   select(taxon = `Feature ID`, Family) %>% 
   mutate(Family = ifelse(is.na(Family),taxon, Family)) # If no Family name, keep the feature ID for now
@@ -118,7 +127,6 @@ strat_Family_fast = strat_filt_fast %>%
   summarize(rel_abun = sum(rel_abun)) %>% 
   ungroup()
 
-# Aggregate the ASVs to the Family level
 strat_Family_moderate = strat_filt_moderate %>%
   left_join(tax) %>%
   group_by(Family, `function`, sample, swim_performance) %>% 
@@ -126,7 +134,6 @@ strat_Family_moderate = strat_filt_moderate %>%
   summarize(rel_abun = sum(rel_abun)) %>% 
   ungroup()
 
-# Aggregate the ASVs to the Family level
 strat_Family_slow = strat_filt_slow %>%
   left_join(tax) %>%
   group_by(Family, `function`, sample, swim_performance) %>% 
@@ -134,6 +141,12 @@ strat_Family_slow = strat_filt_slow %>%
   summarize(rel_abun = sum(rel_abun)) %>% 
   ungroup()
 
+strat_Family = strat_filt %>%
+  left_join(tax) %>%
+  group_by(Family, `function`, sample, swim_performance) %>% 
+  # This is the 'amount' of the function contributed by each Family
+  summarize(rel_abun = sum(rel_abun)) %>% 
+  ungroup()
 
 # PART 2 - Calculating the contributions of each taxon to each function
 
@@ -154,6 +167,11 @@ strat_avg_slow = strat_Family_slow %>%
   summarize(total_contrib_per_Family = sum(rel_abun)) %>% 
   ungroup()
 
+strat_avg = strat_Family %>% 
+  group_by(Family, `function`) %>% 
+  # combine all the relative abundances for a given taxon and pathway across all samples to get an average contribution of that taxon to that pathway
+  summarize(total_contrib_per_Family = sum(rel_abun)) %>% 
+  ungroup()
 
 # Now we just have three columns: the Family, the function, and the function's abundance that belongs to that taxon.
 # Next, we figure out the proportion of each function that belongs to each taxon.
@@ -173,6 +191,12 @@ strat_prop_slow = strat_avg_slow %>%
   group_by(`function`) %>%
   mutate(prop_tax_reads_per_function = total_contrib_per_Family/sum(total_contrib_per_Family)) %>% 
   ungroup() %>% 
+  select(-total_contrib_per_Family) 
+
+strat_prop = strat_avg %>% 
+  group_by(`function`) %>%
+  mutate(prop_tax_reads_per_function = total_contrib_per_Family/sum(total_contrib_per_Family)) %>% 
+  ungroup() %>% 
   select(-total_contrib_per_Family) # We don't need the total contribution per Family anymore, just the proportion
 
 # Sanity check. At this point, the abundances for each function should add up to 1, 
@@ -189,6 +213,10 @@ strat_prop_slow %>% group_by(`function`) %>%
   summarize(sum = sum(prop_tax_reads_per_function)) %>% 
   ungroup()
 
+strat_prop %>% group_by(`function`) %>% 
+  summarize(sum = sum(prop_tax_reads_per_function)) %>% 
+  ungroup()
+
 # PART 3 - get a feel for the data
 
 # Filter for taxa that contribute at least 33% of the reads to a given pathway
@@ -201,16 +229,21 @@ strat_cutoff_moderate = strat_prop_moderate %>%
 strat_cutoff_slow = strat_prop_slow %>% 
   filter(prop_tax_reads_per_function >= 0.33) # None!
 
+strat_cutoff = strat_prop %>% 
+  filter(prop_tax_reads_per_function >= 0.33) # None!
+
 # Top hits:
 head(strat_prop_fast %>% arrange(-prop_tax_reads_per_function))
 head(strat_prop_moderate %>% arrange(-prop_tax_reads_per_function))
 head(strat_prop_slow %>% arrange(-prop_tax_reads_per_function))
+head(strat_prop %>% arrange(-prop_tax_reads_per_function))
 
 # Histogram of the proportions, just to see the distributions. 
 # No clear high-contributing taxa - a taxonomic bar plot-style figure might work best.
 hist(log10(strat_prop_fast$prop_tax_reads_per_function),breaks=50)
 hist(log10(strat_prop_moderate$prop_tax_reads_per_function),breaks=50)
 hist(log10(strat_prop_slow$prop_tax_reads_per_function),breaks=50)
+hist(log10(strat_prop$prop_tax_reads_per_function),breaks=50)
 
 # Part 4 - Visualizing taxonomic bar plot 
 
@@ -239,6 +272,16 @@ strat_prop_grouped_moderate <- strat_prop_moderate %>%
             .groups = "drop")
 
 strat_prop_grouped_slow <- strat_prop_slow %>%
+  group_by(`function`) %>%
+  mutate(Family = ifelse(prop_tax_reads_per_function < threshold,
+                         "<5% of reads",
+                         Family)) %>%
+  ungroup() %>%
+  group_by(`function`, Family) %>%
+  summarize(prop_tax_reads_per_function = sum(prop_tax_reads_per_function),
+            .groups = "drop")
+
+strat_prop_grouped <- strat_prop %>%
   group_by(`function`) %>%
   mutate(Family = ifelse(prop_tax_reads_per_function < threshold,
                          "<5% of reads",
@@ -284,6 +327,18 @@ plot_avg_slow <- strat_prop_grouped_slow %>%
   xlab(NULL)
 
 plot_avg_slow
+
+plot_avg <- strat_prop_grouped %>%
+  mutate(`function` = str_to_title(`function`)) %>%
+  ggplot(aes(`function`, prop_tax_reads_per_function, fill = Family)) +
+  labs(y = "Proportion of Family taxa reads per function") +
+  geom_col(position = "stack", color = NA) +
+  theme_classic(base_size = 22) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        legend.position = "none")+
+  xlab(NULL)
+
+plot_avg
 
 # Plot with a legend, as well as convert an unclassified Family taxa 
 
@@ -337,3 +392,18 @@ plot_avg_slow <- strat_prop_grouped_slow %>%
   xlab(NULL)
 
 plot_avg_slow
+
+plot_avg <- strat_prop_grouped %>%
+  mutate(`function` = str_to_title(`function`)) %>%
+  ggplot(aes(`function`, prop_tax_reads_per_function, fill = Family)) +
+  labs(y = "Proportion of Family taxa reads per function") +
+  geom_col(position = "stack", color = NA) +
+  theme_classic(base_size = 22) +
+  theme(strip.text = element_blank(),
+        axis.text.x = element_text(angle = 45, hjust = 1),
+        legend.title = element_blank(),
+        legend.key.size = unit(1, "cm"),
+        legend.text = element_text(size = 16)) +
+  xlab(NULL)
+
+plot_avg
